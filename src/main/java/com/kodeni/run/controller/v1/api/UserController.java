138ad29930e664515184272a94e38fbd080efc57
yilmazoguzhan <oguzhaanyilmaz@gmail.com>
2020-02-13 20:12:50 +0300

    Added files.

---
+++ src/main/java/com/kodeni/run/controller/v1/api/UserController.java
@@ -0,0 +1,20 @@
+package com.kodeni.run.controller.v1.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.Response;
+
+@RestController
+@RequestMapping("api/v1/user")
+public class UserController {
+
+    @PostMapping("/signin")
+    public ResponseEntity<String> SignIn(String username)
+    {
+        return new ResponseEntity<String>("Hi " + username, HttpStatus.OK);
+    }
+}
