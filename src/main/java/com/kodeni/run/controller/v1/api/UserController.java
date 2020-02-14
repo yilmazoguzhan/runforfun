393a20c30bddab95dd0787a108297191568218c3
yilmazoguzhan <oguzhaanyilmaz@gmail.com>
2020-02-14 16:14:41 +0300

    Added swagger.

--- src/main/java/com/kodeni/run/controller/v1/api/UserController.java
+++ src/main/java/com/kodeni/run/controller/v1/api/UserController.java
@@ -3,19 +3,16 @@ package com.kodeni.run.controller.v1.api;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.xml.ws.Response;
 
 @RestController
 @RequestMapping("api/v1/user")
 public class UserController {
 
-    @GetMapping("/signin")
-    public ResponseEntity<String> SignIn(String username)
+    @GetMapping("/")
+    public ResponseEntity<String> SignIn()
     {
-        return new ResponseEntity<String>("Hi " + username, HttpStatus.OK);
+        return new ResponseEntity<String>("Hi " + " sadf", HttpStatus.OK);
     }
 }
